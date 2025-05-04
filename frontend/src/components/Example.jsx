a80754b4ba43a44e84697fbf1d0a2130fbd03899
franSantos10 <franci.santos.silva10@gmail.com>
2025-05-04 10:57:03 -0300

    Configuração do projeto  frontend

---
+++ frontend/src/components/Example.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function ExampleComponent() {
+  return (
+    <div>
+      Exemplo de Componente
+    </div>
+  );
+}
+
+export default ExampleComponent;
+
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Me Adotta 🐾</h1>
+      <p>Conectando animais e pessoas!</p>
+    </div>
+  );
+}
+
+export default Home;
+
