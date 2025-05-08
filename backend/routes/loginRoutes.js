cf62ea1bd87e4d21e908ae9bae69611ab1ffbd05
franSantos10 <franci.santos.silva10@gmail.com>
2025-05-08 14:17:33 -0300

    Refatorado a rota de login para usar consulta de banco de dados para autenticação

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -1,18 +1,26 @@
+// login.js
 const express = require('express');
 const router = express.Router();
+const pool = require('../db');
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const { email, senha } = req.body;
 
-  // Simulação de protetor
-  if (email === 'franci.santos.silva10@gmail.com' && senha === '123456' ||
-    email === 'protetor@gmail.com' && senha === '123456'
-  ) {
-    return res.status(200).json({ token: 'protetor-auth',  email: email }); // pode usar JWT depois
-  } else {
-    return res.status(401).json({ erro: 'Credenciais inválidas' });
+  try {
+    const result = await pool.query(
+      'SELECT * FROM usuarios WHERE email = $1 AND senha = $2',
+      [email, senha]
+    );
+
+    if (result.rows.length > 0) {
+      return res.status(200).json({ token: 'protetor-auth', email });
+    } else {
+      return res.status(401).json({ erro: 'Credenciais inválidas' });
+    }
+  } catch (err) {
+    console.error('Erro ao buscar usuário:', err);
+    return res.status(500).json({ erro: 'Erro interno do servidor' });
   }
 });
-
 
 module.exports = router;
